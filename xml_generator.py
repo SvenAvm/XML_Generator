bb7a1401518336777c3c2a71d8530428602c1785
buff3r <sven.avm@gmail.com>
2022-05-10 12:11:51 +0200

    saving progress

--- xml_generator.py
+++ xml_generator.py
@@ -7,14 +7,25 @@ date = date.today()
 
 
 def generate_xml(filename):
-    root = xml.Element("XML_LOG")
-    c1 = xml.Element("LOG")
-    root.append(c1)
-    date1 = xml.SubElement(c1, "DATE")
+    root = xml.Element("LOG")
+    root.append(root)
+
+    date1 = xml.SubElement(root, "DATE")
     date1.text = f"{date}"
-    time1 = xml.SubElement(c1, "TIME")
+
+    time1 = xml.SubElement(root, "TIME")
     time1.text = f"{random.choice(times)}"
-    cc1 = xml.SubElement(c1, "CC")
+
+    cc1 = xml.SubElement(root, "CC")
     cc1.text = f"{random.choice(cc_list)}"
-    amount1 = xml.SubElement(c1, "AMOUNT")
+
+    amount1 = xml.SubElement(root, "AMOUNT")
     amount1.text = f"{random.choice(amounts)}"
+
+    tree = xml.ElementTree(root)
+    with open(filename, "wb") as files:
+        tree.write(files)
+
+
+if __name__ == "__main__":
+    generate_xml("test.xml")
