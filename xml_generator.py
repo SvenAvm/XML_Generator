ea766ac7ca05ce845f872314dac13459feb62c35
buff3r <sven.avm@gmail.com>
2022-05-10 16:04:23 +0200

    Generator Complete

--- xml_generator.py
+++ xml_generator.py
@@ -1,34 +1,46 @@
+import xml.etree.ElementTree as ET
 from values_generator import times, cc_list, amounts
 from datetime import date
-import xml.etree.ElementTree as xml
-import random
-
-date = date.today()
 
 
-def generate_xml(filename):
-    root = xml.Element("LOG")
-    root.append(root)
-
-    date1 = xml.SubElement(root, "DATE")
-    date1.text = f"{date}"
-
-    time1 = xml.SubElement(root, "TIME")
-    time1.text = f"{random.choice(times)}"
-
-    cc1 = xml.SubElement(root, "CC")
-    cc1.text = f"{random.choice(cc_list)}"
-
-    amount1 = xml.SubElement(root, "AMOUNT")
-    amount1.text = f"{random.choice(amounts)}"
-
-    tree = xml.ElementTree(root)
-
-    with open(filename, "wb") as files:
-        tree.write(files)
+current_date = date.today()
+log_iterations_list = []
+iterations_required = 10000
+log_iterations_counter = 0
+while len(log_iterations_list) < 10000:
+    log_iterations_counter += 1
+    log_iterations_list.append(log_iterations_counter)
 
 
-if __name__ == "__main__":
-    generate_xml("test.xml")
+def create_xml():
+    function_iterations_counter = 0
+    with open('logs.xml', 'a', encoding='utf-8') as xml_file:
 
-generate_xml("text.xml")
+        while function_iterations_counter < 10000:
+            # we make root element
+            log = ET.Element("LOG")
+
+            # create sub element
+            log = ET.SubElement(log, "log")
+
+            date = ET.SubElement(log, "DATE")
+            date.text = str(current_date)
+
+            time = ET.SubElement(log, "TIME")
+            time.text = str(times[function_iterations_counter])
+
+            cc = ET.SubElement(log, "CC")
+            cc.text = str(cc_list[function_iterations_counter])
+
+            amount = ET.SubElement(log, "AMOUNT")
+            amount.text = str(amounts[function_iterations_counter])
+
+            tree = ET.ElementTree(log)
+            function_iterations_counter += 1
+
+            tree.write(xml_file, encoding='unicode', xml_declaration=True)
+
+
+create_xml()
+
+
